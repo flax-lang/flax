fb796e5043d175179a8fe6cdba5926d1a56b2183
zhiayang <zhiayang@gmail.com>
2015-02-17 23:24:07 +0800

    get { } works!

--- source/Codegen/UnaryOpCodegen.cpp
+++ source/Codegen/UnaryOpCodegen.cpp
@@ -9,7 +9,7 @@
 using namespace Ast;
 using namespace Codegen;
 
-Result_t UnaryOp::codegen(CodegenInstance* cgi, llvm::Value* lhsPtr)
+Result_t UnaryOp::codegen(CodegenInstance* cgi, llvm::Value* lhsPtr, llvm::Value* rhs)
 {
 	assert(this->expr);
 	Result_t res = this->expr->codegen(cgi);
