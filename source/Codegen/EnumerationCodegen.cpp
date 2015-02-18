5a50bd002d2ad63ed8f99fe9dab5a0df0a8c12b7
zhiayang <zhiayang@gmail.com>
2015-02-18 23:16:15 +0800

    enums mostly work... like C enums, which is a Bad Thing (tm)

--- source/Codegen/EnumerationCodegen.cpp
+++ source/Codegen/EnumerationCodegen.cpp
@@ -9,6 +9,34 @@
 using namespace Ast;
 using namespace Codegen;
 
+namespace Codegen
+{
+	Result_t enumerationAccessCodegen(CodegenInstance* cgi, Expr* lhs, Expr* rhs)
+	{
+		VarRef* enumName = dynamic_cast<VarRef*>(lhs);
+		VarRef* caseName = dynamic_cast<VarRef*>(rhs);
+		assert(enumName);
+
+		if(!caseName)
+			error(rhs, "Expected identifier after enumeration access");
+
+		TypePair_t* tp = cgi->getType(enumName->name);
+		assert(tp);
+		assert(tp->second.second == ExprType::Enum);
+
+		Enumeration* enr = dynamic_cast<Enumeration*>(tp->second.first);
+		assert(enr);
+
+		for(auto p : enr->cases)
+		{
+			if(p.first == caseName->name)
+				return p.second->codegen(cgi);
+		}
+
+		error(rhs, "Enum '%s' has no such case '%s'", enumName->name.c_str(), caseName->name.c_str());
+	}
+}
+
 Result_t Enumeration::codegen(CodegenInstance* cgi, llvm::Value* lhsPtr, llvm::Value* rhs)
 {
 	return Result_t(0, 0);
@@ -16,5 +44,24 @@ Result_t Enumeration::codegen(CodegenInstance* cgi, llvm::Value* lhsPtr, llvm::V
 
 void Enumeration::createType(CodegenInstance* cgi)
 {
+	// make sure all types are the same
+	// todo: remove this limitation maybe?
 
+	llvm::Type* prev = 0;
+	for(auto pair : this->cases)
+	{
+		if(!prev)
+			prev = cgi->getLlvmType(pair.second);
+
+
+		llvm::Type* t = cgi->getLlvmType(pair.second);
+		if(t != prev)
+			error(pair.second, "Enumeration values must have the same type, have %s: %s and %s", pair.first.c_str(), cgi->getReadableType(pair.second).c_str(), cgi->getReadableType(prev).c_str());
+
+		prev = t;
+	}
+
+
+	// now that they're all the same type:
+	cgi->addNewType(prev, this, ExprType::Enum);
 }
