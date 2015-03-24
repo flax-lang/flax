559c488d1f0437afa101ee837239c2251e1c38e2
zhiayang <zhiayang@gmail.com>
2015-03-24 22:59:08 +0800

    fuck

--- build/get-dependencies.sh
+++ build/get-dependencies.sh
@@ -2,7 +2,7 @@
 
 wget "http://capri.ghostkernel.org/downloads/capri-2.1.0-src.tar.gz"
 tar -xf capri-2.1.0-src.tar.gz
-cd capri-2.1.0-src
+cd capri-2.1.0-src/capri-cli
 sudo sh build.sh all install
 
 cd ..
