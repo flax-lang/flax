9dec192815fb01de2f64a38613e3ac8f46fef10c
zhiayang <zhiayang@gmail.com>
2015-03-24 22:57:46 +0800

    fix capri

--- build/get-dependencies.sh
+++ build/get-dependencies.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-CAPRI_VERSION=1.17
-CAPRI_RELEASE_URL="http://91.206.143.88/capri/releases/"
-echo "Getting Capri version $CAPRI_VERSION from $CAPRI_RELEASE_URL"
+wget "http://capri.ghostkernel.org/downloads/capri-2.1.0-src.tar.gz"
+tar -xf capri-2.1.0-src.tar.gz
+cd capri-2.1.0-src
+sudo sh build.sh all install
 
-if [ `uname` = "Darwin" ]; then
-	# special case, get 1.18 for OS X
-	wget "$CAPRI_RELEASE_URL/1.18/capri-1.18.2-mac-x64.zip"
-	mv capri-1.18.2-mac-x64.zip ./capri.zip
-	unzip capri.zip
-	rm capri.zip
-
-elif [ `uname` = "Linux" ]; then
-	wget "$CAPRI_RELEASE_URL/$CAPRI_VERSION/capri-1.17-linux-x64.tar.gz"
-	mv capri-1.17-linux-x64.tar.gz ./capri.tar.gz
-	tar -xf capri.tar.gz
-
-	rm capri.tar.gz
-fi
+cd ..
 
 
