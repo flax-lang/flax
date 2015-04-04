f47607204587eaec40bc9854a94cf56ad9b86859
zhiayang <zhiayang@gmail.com>
2015-04-04 22:21:28 +0800

    sorry max, but getting capri to do concurrent stuff is a pain.

---
+++ shakefile.hs
@@ -0,0 +1,86 @@
+-- shakefile.hs
+-- Copyright (c) 2014 - The Foreseeable Future, zhiayang@gmail.com
+-- Licensed under the Apache License Version 2.0.
+
+import System.Exit
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
+main :: IO ()
+
+sysroot		= "build/sysroot"
+prefix		= "usr/local"
+outputBin	= "flaxc"
+
+finalOutput	= sysroot </> prefix </> "bin" </> outputBin
+
+
+
+llvmConfig	= "llvm-config"
+disableWarn	= "-Wno-unused-parameter -Wno-sign-conversion -Wno-padded -Wno-c++98-compat -Wno-weak-vtables -Wno-documentation-unknown-command -Wno-old-style-cast -Wno-c++98-compat-pedantic -Wno-conversion -Wno-shadow -Wno-global-constructors -Wno-exit-time-destructors -Wno-missing-noreturn -Wno-unused-macros -Wno-switch-enum -Wno-deprecated -Wno-shift-sign-overflow -Wno-format-nonliteral -Wno-gnu-zero-variadic-macro-arguments -Wno-trigraphs"
+
+cxxFlags	= "-std=gnu++1z -g -Wall -Weverything " ++ disableWarn ++ " -frtti -fexceptions -fno-omit-frame-pointer -I`" ++ llvmConfig ++ " --includedir`"
+
+llvmConfigInvoke	= "`" ++ llvmConfig ++ " --cxxflags --ldflags --system-libs --libs core jit native bitwriter`"
+
+
+
+compiledTest		= "build/test"
+testSource			= "build/test.flx"
+flaxcFlags			= "-O3 -no-lowercase-builtin -o " ++ compiledTest ++ " -sysroot '" ++ sysroot ++ "'"
+
+
+main = shakeArgs shakeOptions{shakeFiles="build"} $ do
+	want [finalOutput]
+	want [compiledTest]
+
+	phony "clean" $ do
+		putNormal "Cleaning files"
+		removeFilesAfter "source" ["//*.o"]
+
+
+	compiledTest %> \out -> do
+		alwaysRerun
+		Exit code <- cmd Shell finalOutput [flaxcFlags] testSource
+
+		putNormal "======================================="
+		cmd Shell (if code == ExitSuccess then [compiledTest] else ["echo Test failed"])
+
+
+	finalOutput %> \out -> do
+		cs <- getDirectoryFiles "" ["source//*.cpp"]
+		let os = [c ++ ".o" | c <- cs]
+		need os
+
+		putNormal finalOutput
+		cmd Shell "clang++ -o" [out] [llvmConfigInvoke] os
+
+
+
+	"source//*.cpp.o" %> \out -> do
+		let c = dropExtension out
+		let m = out ++ ".m"
+		() <- cmd Shell "clang++ -c" [c] [cxxFlags] "-o" [out] "-MMD -MF" [m]
+		needMakefileDependencies m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
