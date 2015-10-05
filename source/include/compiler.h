1d0a033ef2f50b0ac8545761a0007f1eae6b1b81
zhiayang <zhiayang@gmail.com>
2015-10-05 13:02:27 +0800

    got custom operator overloads working, mostly

--- source/include/compiler.h
+++ source/include/compiler.h
@@ -23,7 +23,7 @@ namespace Parser
 namespace Compiler
 {
 	std::tuple<Ast::Root*, std::vector<std::string>, std::unordered_map<std::string, Ast::Root*>, std::unordered_map<std::string, llvm::Module*>>
-	compileFile(std::string filename);
+	compileFile(std::string filename, std::map<Ast::ArithmeticOp, std::pair<std::string, int>> foundOps, std::map<std::string, Ast::ArithmeticOp> foundOpsRev);
 
 	// final stages
 	void compileProgram(llvm::Module* module, std::vector<std::string> filelist, std::string foldername, std::string outname);
