4c48161a75b8fdf4b94b73a338e43ef591043008
zhiayang <zhiayang@gmail.com>
2015-10-06 22:43:53 +0800

    so. given up on proper, actual custom operators. all custom ops with the same identifier will be forced to use the first found precedence. this is entirely a parser issue, codegen works fine

---
+++ source/SemanticAnalysis/ParserTypeAnalysis.cpp
@@ -0,0 +1,12 @@
+// ParserTypeAnalysis.cpp
+// Copyright (c) 2014 - The Foreseeable Future, zhiayang@gmail.com
+// Licensed under the Apache License Version 2.0.
+
+#include "../include/ast.h"
+#include "../include/codegen.h"
+#include "../include/semantic.h"
+
+using namespace Ast;
+using namespace Codegen;
+
+// todo.
