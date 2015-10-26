a6ab91cae9659582e5845f21b974646dc422d9cc
zhiayang <zhiayang@gmail.com>
2015-10-26 14:26:55 +0800

    nothing in particular
    Former-commit-id: 46bb5e36cf11f07a0e4d6d0d3f1c71d6879e2be4

--- source/TypeInfo/TypeInfo.cpp
+++ source/TypeInfo/TypeInfo.cpp
@@ -2,9 +2,8 @@
 // Copyright (c) 2014 - 2015, zhiayang@gmail.com
 // Licensed under the Apache License Version 2.0.
 
-#include "ast.h"
 #include "codegen.h"
-
+#include "typeinfo.h"
 
 using namespace Ast;
 using namespace Codegen;
