ee48aebeb3607147a1a27bf3a3404f81d04f9582
zhiayang <zhiayang@gmail.com>
2016-07-20 00:12:15 +0800

    2x perf increase by using hashmap? woah

--- source/include/ast.h
+++ source/include/ast.h
@@ -93,6 +93,28 @@ struct Identifier
 	Identifier(std::string _name, std::deque<std::string> _scope, IdKind _kind) : name(_name), scope(_scope), kind(_kind) { }
 };
 
+namespace std
+{
+	template<>
+	struct hash<Identifier>
+	{
+		std::size_t operator()(const Identifier& k) const
+		{
+			using std::size_t;
+			using std::hash;
+			using std::string;
+
+			// Compute individual hash values for first,
+			// second and third and combine them using XOR
+			// and bit shifting:
+
+			// return ((hash<string>()(k.name) ^ (hash<std::deque<std::string>>()(k.scope) << 1)) >> 1) ^ (hash<int>()(k.third) << 1);
+			return hash<string>()(k.str());
+		}
+	};
+}
+
+
 
 
 
