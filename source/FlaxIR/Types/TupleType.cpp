150efa46d0eeb34fbbb29e9efecd091af64f310e
zhiayang <zhiayang@gmail.com>
2016-07-26 23:07:48 +0800

    lol

--- source/FlaxIR/Types/TupleType.cpp
+++ source/FlaxIR/Types/TupleType.cpp
@@ -1,5 +1,5 @@
 // TupleType.cpp
-// Copyright (c) 2014 - The Foreseeable Future, zhiayang@gmail.com
+// Copyright (c) 2014 - 2016, zhiayang@gmail.com
 // Licensed under the Apache License Version 2.0.
 
 #include "errors.h"
