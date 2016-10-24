37128b5224b902f66405b6cff5772b0b97020349
zhiayang <zhiayang@gmail.com>
2016-10-25 04:23:06 +0800

    added support for unary precedence (i think), meaning &arr[0] now parses as &(arr[0]), aka c-style, aka properly.
    apparently i forget the features i have, because array-to-pointer casting was already supported for some time now.
    
    
    Former-commit-id: 236b7f001b77201bcd49f9aabd4ff4b558c170a6

--- source/Codegen/Operators/Others.cpp
+++ source/Codegen/Operators/Others.cpp
@@ -147,9 +147,6 @@ namespace Operators
 			&& lhs->getType()->toArrayType()->getElementType() == rtype->getPointerElementType())
 		{
 			// array to pointer cast.
-			if(!dynamic_cast<fir::ConstantArray*>(lhs))
-				warn(user, "Casting from non-constant array to pointer is potentially unsafe");
-
 			iceAssert(lhsPtr);
 
 			fir::Value* lhsRawPtr = cgi->irb.CreateConstGEP2(lhsPtr, 0, 0);
