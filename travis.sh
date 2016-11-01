25c99c18b756c419a8d78a4eb43d8cad287a0e29
zhiayang <zhiayang@gmail.com>
2016-11-01 14:23:33 +0800

    fk
    Former-commit-id: 6537b735fe9ee54c27f31326f872cd36b3a5aa10

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-CXX=clang++-3.8 CC=clang-3.8 WARNINGS=-Wnounknown-warning-option make -j2
+CXX="clang++-3.8" CC="clang-3.8" WARNINGS="-Wno-unknown-warning-option" CXXFLAGS="-stdlib=libc++" make -j2
