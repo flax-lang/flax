3416917b21181af2f30f7f3ea6552544053d87a7
zhiayang <zhiayang@gmail.com>
2016-11-01 13:53:49 +0800

    let's see.
    Former-commit-id: 32199c808fcf0eb39b691771aeb5a600b0819cad

--- travis.sh
+++ travis.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-CLANGCXX=`which clang++`
-CLANGC=`which clang`
-echo $CLANGCXX
-echo $CLANGC
-echo `which clang++-3.7`
-sudo mv $CLANGCXX /tmp/clang++-old
-sudo mv $CLANGC /tmp/clang-old
-sudo cp `which clang++-3.7` $CLANGCXX
-sudo cp `which clang-3.7` $CLANGC
-b2 -j2
+CXX=clang++-3.8 CC=clang-3.8 make -j2
