b13eb12834201dc001bd913bf1a203f459f33885
zhiayang <zhiayang@gmail.com>
2017-02-03 02:39:43 +0800

    boilerplate in place (does not link, missing stuff)
    Former-commit-id: 9c5d13f03a698105b9bd36c1f808f67c77fc82c1

--- source/include/ast.h
+++ source/include/ast.h
@@ -243,6 +243,24 @@ namespace Ast
 		fir::Type* concretisedType = 0;
 	};
 
+	struct DestructuredTupleDecl : Expr
+	{
+		~DestructuredTupleDecl();
+		DestructuredTupleDecl(const Parser::Pin& pos) : Expr(pos) { }
+
+		virtual Result_t codegen(Codegen::CodegenInstance* cgi, fir::Value* extra = 0) override;
+		virtual fir::Type* getType(Codegen::CodegenInstance* cgi, bool allowFail = false, fir::Value* extra = 0) override;
+
+		bool immutable = false;
+		Expr* rightSide = 0;
+	};
+
+
+
+
+
+
+
 	struct BracedBlock;
 	struct ComputedProperty : VarDecl
 	{
