3231156aaef5c62ebfe40eb08759518eb69d7d10
zhiayang <zhiayang@gmail.com>
2017-11-19 17:39:01 +0800

    some windows work...?

--- source/include/precompile.h
+++ source/include/precompile.h
@@ -21,7 +21,20 @@
 #include <algorithm>
 #include <functional>
 #include <unordered_map>
-#include <experimental/string_view>
+
+#ifndef __has_include
+#error "Please switch to a compiler that supports '__has_include'"
+#endif
+
+#if __has_include(<string_view>)
+	#include <string_view>
+	#define STRING_VIEW_IS_EXP 0
+#elif __has_include(<experimental/string_view>)
+	#include <experimental/string_view>
+	#define STRING_VIEW_IS_EXP 1
+#else
+	#error "Please switch to a compiler that supports 'string_view', or change your c++ standard version"
+#endif
 
 #include "../external/mpreal/mpreal.h"
 
