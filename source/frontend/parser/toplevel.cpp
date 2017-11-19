88ce9e462c187ae7c2513aeda658e21e4094e45b
zhiayang <zhiayang@gmail.com>
2017-11-19 17:59:48 +0800

    more fixes

--- source/frontend/parser/toplevel.cpp
+++ source/frontend/parser/toplevel.cpp
@@ -45,7 +45,7 @@ namespace parser
 				if(tokens[i].type != TT::NewLine && tokens[i].type != TT::Semicolon && tokens[i].type != TT::Comment)
 				{
 					error(tokens[i].loc, "Expected newline or semicolon to terminate import statement, found '%s'",
-						tokens[i].text.to_string());
+						tokens[i].str());
 				}
 
 				// i++ handled by loop
