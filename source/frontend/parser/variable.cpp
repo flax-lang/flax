d4f1d606b3f163c8d43054dda84462f1c56fe2f7
zhiayang <zhiayang@gmail.com>
2018-01-14 01:44:21 +0800

    implement parsing for arbitrarily-nested destructuring statements, and streamline the existing implementation to be fully recursionably-magiced-no-code-duplication-thingy

--- source/frontend/parser/variable.cpp
+++ source/frontend/parser/variable.cpp
@@ -10,82 +10,164 @@ using namespace lexer;
 
 namespace parser
 {
-	std::map<size_t, std::tuple<std::string, bool, Location>> parseArrayDecomp(State& st)
+	// std::vector<ast::DecompMapping> parseArrayDecomp(State& st)
+	// {
+	// 	using TT = lexer::TokenType;
+	// 	iceAssert(st.front() == TT::LSquare);
+	// 	st.pop();
+
+	// 	std::vector<ast::DecompMapping> ret;
+
+	// 	size_t index = 0;
+	// 	while(st.front() != TT::RSquare)
+	// 	{
+	// 		std::string id;
+	// 		bool ref = false;
+
+	// 		if(st.front() == TT::Ampersand)
+	// 			ref = true, st.pop();
+
+	// 		if(st.front() == TT::LParen)
+	// 		{
+	// 			error(st, "Nesting tuple bindings inside array decompositions is not supported yet");
+	// 		}
+	// 		else if(st.front() == TT::Identifier)
+	// 		{
+	// 			mapping[index] = std::make_tuple(id, ref, st.loc());
+	// 			index++;
+
+	// 			if(ref && id == "_")
+	// 				error(st, "Invalid combination of '&' and '_' in array decomposition");
+
+	// 			st.eat();
+	// 		}
+	// 		else if(st.front() == TT::Ellipsis)
+	// 		{
+	// 			st.eat();
+
+	// 			// check if we're binding
+	// 			if(st.front() == TT::Ampersand || st.front() == TT::Identifier)
+	// 			{
+	// 				// yes we are
+	// 				if(st.front() == TT::Ampersand)
+	// 				{
+	// 					ref = true;
+	// 					st.pop();
+	// 					if(st.front() != TT::Identifier || st.front().str() == "_")
+	// 						expectedAfter(st, "identifier", "'&' in binding", st.front().str());
+	// 				}
+
+	// 				iceAssert(st.front() == TT::Identifier);
+	// 				id = st.front().str();
+
+	// 				mapping[-1] = std::make_tuple(id, ref, st.loc());
+	// 				st.pop();
+	// 			}
+	// 			else if(st.front() != TT::RSquare)
+	// 			{
+	// 				error("Ellipsis binding must be last in decomposition, found '%s' instead of expected ']'", st.front().str().c_str());
+	// 			}
+	// 		}
+	// 		else
+	// 		{
+	// 			error(st, "Unexpected token '%s' in array decomposition", st.front().str());
+	// 		}
+
+	// 		if(st.front() == TT::Comma)
+	// 		{
+	// 			st.eat();
+	// 			if(st.front() == TT::RSquare)
+	// 				error(st, "Trailing commas are not allowed");
+
+	// 			continue;
+	// 		}
+	// 	}
+
+	// 	iceAssert(st.eat() == TT::RSquare);
+	// 	return mapping;
+	// }
+
+
+
+	static ast::DecompMapping recursivelyParseDecomp(State& st)
 	{
 		using TT = lexer::TokenType;
-		iceAssert(st.front() == TT::LSquare);
-		st.pop();
+		ast::DecompMapping outer;
 
-		std::map<size_t, std::tuple<std::string, bool, Location>> mapping;
+		iceAssert(st.front() == TT::LSquare || st.front() == TT::LParen);
+		if(st.pop() == TT::LSquare)
+			outer.array = true;
 
-		size_t index = 0;
-		while(st.front() != TT::RSquare)
+		bool didRest = false;
+		debuglog("enter\n");
+
+		while(st.front() != (outer.array ? TT::RSquare : TT::RParen))
 		{
-			std::string id;
-			bool ref = false;
+			ast::DecompMapping inside;
 
 			if(st.front() == TT::Ampersand)
-				ref = true, st.pop();
+				inside.ref = true, st.pop();
 
-			if(st.front() == TT::Underscore || (st.front() == TT::Identifier && st.front().str() == "_"))
+			if(st.front() == TT::LParen || st.front() == TT::LSquare)
 			{
-				id = "_";
-				st.pop();
+				if(inside.ref) error(st, "Cannot bind by reference in nested decomposition; modify the binding type for each identifier");
 
-				if(ref) error(st, "Invalid combination of '&' and '_' in array decomposition");
-			}
-			else if(st.front() == TT::Identifier)
-			{
-				mapping[index] = std::make_tuple(id, ref, st.loc());
-				index++;
-
-				st.eat();
+				inside = recursivelyParseDecomp(st);
 			}
 			else if(st.front() == TT::Ellipsis)
 			{
-				st.eat();
+				if(!outer.array)    error(st, "'...' cannot be used in a tuple destructure");
+				else if(didRest)    error(st, "'...' must be the last binding in an array destructure");
+				else if(inside.ref) error(st, "Invalid use of '&' before '...' binding");
 
-				// check if we're binding
-				if(st.front() == TT::Ampersand || st.front() == TT::Identifier)
-				{
-					// yes we are
-					if(st.front() == TT::Ampersand)
-					{
-						ref = true;
-						st.pop();
-						if(st.front() != TT::Identifier || st.front().str() == "_")
-							expectedAfter(st, "identifier", "'&' in binding", st.front().str());
-					}
+				st.pop();
+				if(st.front() == TT::Ampersand)
+					outer.restRef = true, st.pop();
 
-					iceAssert(st.front() == TT::Identifier);
-					id = st.front().str();
+				if(st.front() == TT::Identifier)
+					outer.restName = st.front().str(), st.pop();
 
-					mapping[-1] = std::make_tuple(id, ref, st.loc());
-					st.pop();
-				}
-				else if(st.front() != TT::RSquare)
-				{
-					error("Ellipsis binding must be last in decomposition, found '%s' instead of expected ']'", st.front().str().c_str());
-				}
+				// else
+				// 	expected(st, "binding for rest of array after '...' in destructuring declaration", st.front().str());
+
+				if(outer.restName == "_" && outer.restRef)
+					error(st.ploc(), "Invalid combination of '_' and '&'");
+
+				didRest = true;
+			}
+			else if(st.front() == TT::Identifier)
+			{
+				inside.name = st.pop().str();
+				if(inside.name == "_" && inside.ref)
+					error(st.loc(), "Invalid combination of '_' and '&'");
+
+				outer.inner.push_back(inside);
+
+				debuglog("found '%s'\n", inside.name);
 			}
 			else
 			{
-				error(st, "Unexpected token '%s' in array decomposition", st.front().str());
+				expected(st.loc(), "identifier or '...' in destructuring declaration", st.front().str());
 			}
+
+			if(st.front() != TT::Comma && st.front() != (outer.array ? TT::RSquare : TT::RParen))
+				expected(st, "')', ']' or ',' in destructuring declaration", st.front().str());
 
 			if(st.front() == TT::Comma)
-			{
-				st.eat();
-				if(st.front() == TT::RSquare)
-					error(st, "Trailing commas are not allowed");
-
-				continue;
-			}
+				st.pop();
 		}
 
-		iceAssert(st.eat() == TT::RSquare);
-		return mapping;
+		if(outer.array && !didRest)
+			error(st, "'...' is mandatory for array destructuring, regardless of binding");
+
+		iceAssert(st.front() == (outer.array ? TT::RSquare : TT::RParen));
+		st.pop();
+
+		debuglog("leave\n");
+		return outer;
 	}
+
+
 
 	ArrayDecompVarDefn* parseArrayDecompDecl(State& st)
 	{
@@ -93,7 +175,7 @@ namespace parser
 		iceAssert(st.front() == TT::LSquare);
 
 		auto decomp = new ArrayDecompVarDefn(st.loc());
-		decomp->mapping = parseArrayDecomp(st);
+		decomp->bindings = recursivelyParseDecomp(st);
 
 		if(st.front() != TT::Equal)
 			expected(st, "'=' for assignment to decomposition", st.front().str());
@@ -104,21 +186,6 @@ namespace parser
 		return decomp;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-	std::vector<ast::TupleDecompMapping> parseTupleDecomp(State& st)
-	{
-		error("unsupported");
-	}
 
 	static ast::TupleDecompVarDefn* parseTupleDecompDecl(State& st)
 	{
@@ -126,7 +193,7 @@ namespace parser
 		iceAssert(st.front() == TT::LParen);
 
 		auto decomp = new TupleDecompVarDefn(st.loc());
-		decomp->mappings = parseTupleDecomp(st);
+		decomp->bindings = parseTupleDecomp(st);
 
 		if(st.front() != TT::Equal)
 			expected(st, "'=' for assignment to decomposition", st.front().str());
@@ -137,6 +204,10 @@ namespace parser
 		return decomp;
 	}
 
+	ast::DecompMapping parseTupleDecomp(State& st)
+	{
+		return recursivelyParseDecomp(st);
+	}
 
 
 
